45f182fbcea382604f6f944892763eb1fd1a743d
Caseres Marina <marinacaseres1997@gmail.com>
2021-10-15 23:16:35 -0300

    Agregado movimiento de agacharse.

--- js/ViewPersonaje.js
+++ js/ViewPersonaje.js
@@ -3,11 +3,39 @@ class ViewPersonaje {
         this.personaje = personaje;
     }
     salta(){
+        this.personaje.classList.remove("agachar");
+        this.personaje.classList.remove("rodar");
         this.personaje.classList.remove("caminar");
         this.personaje.classList.add("saltar");
     }
+
     camina(){
+        this.personaje.classList.remove("levantar");
         this.personaje.classList.remove("saltar");
+        this.personaje.classList.remove("agachar");
+        this.personaje.classList.remove("rodar");
         this.personaje.classList.add("caminar");
+    }
+
+    down(){
+        this.personaje.classList.remove("saltar");
+        this.personaje.classList.remove("rodar");
+        this.personaje.classList.remove("caminar");
+        this.personaje.classList.add("agachar");
+    }
+
+    rodar(){
+        this.personaje.classList.remove("saltar");
+        this.personaje.classList.remove("agachar");
+        this.personaje.classList.remove("caminar");
+        this.personaje.classList.add("rodar");
+    }
+
+    rollUp(){
+        this.personaje.classList.remove("saltar");
+        this.personaje.classList.remove("agachar");
+        this.personaje.classList.remove("caminar");
+        this.personaje.classList.remove("rodar");
+        this.personaje.classList.add("levantar");
     }
 }
--- js/main.js
+++ js/main.js
@@ -2,19 +2,47 @@ document.addEventListener("DOMContentLoaded", function(){
     let personaje= document.getElementById("personaje");
     let viewPersonaje = new ViewPersonaje(personaje);
 
-    document.addEventListener('keydown',saltar);
+    document.addEventListener('keydown',movimiento);
+    //document.addEventListener('keypresed',rodar);
+    //document.addEventListener('keyup',caminar);
 
     let salta = true;
-    function saltar(e) {
+    let down = true;
+    let roll = false;
+
+    function movimiento(e) {
         if(e.key == "ArrowUp" && salta){
-            salta = false;
-            viewPersonaje.salta();
-            setTimeout(caminar,1000);
+            if(roll){
+                //se levanta de rodar
+                roll = false;
+                viewPersonaje.rollUp();
+                setTimeout(caminar,300);
+                //no está rodando
+            }else{
+                //salta
+                salta = false;
+                viewPersonaje.salta();
+                setTimeout(caminar,1000);
+            }
+        }else if(e.key == "ArrowDown" && down && salta){//se pregunta por si está abilitado para saltar si no lo está que no se agache
+            //se agacha
+            down = false;
+            viewPersonaje.down();
+            //empieza a rodar
+            setTimeout(rodar,250);
+        }
+    }
+
+    function rodar(){
+        if(!down){
+            roll = true;
+            viewPersonaje.rodar();
         }
     }
 
     function caminar(){
         salta = true;
+        down = true;
         viewPersonaje.camina();
     }
 });
