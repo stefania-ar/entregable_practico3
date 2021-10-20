79adc0d0cf4a41de9b84899e11c223b31512a5bf
Caseres Marina <marinacaseres1997@gmail.com>
2021-10-20 16:29:53 -0300

    Areglada función de detener animaciones de divs obtáculos.

--- js/control/ControlDivObstaculos.js
+++ js/control/ControlDivObstaculos.js
@@ -16,7 +16,12 @@ class ControlDivObstaculos{
         this.time3=null;
         this.time4=null;
         this.time5=null;
-        this.timeFinal =null;
+
+        this.timeFinal1=null;
+        this.timeFinal2=null;
+        this.timeFinal3=null;
+        this.timeFinal4=null;
+        this.timeFinal5=null;
     }
 
     ubicarDivsEnPantalla(){
@@ -48,7 +53,11 @@ class ControlDivObstaculos{
         clearTimeout(this.time3);
         clearTimeout(this.time4);
         clearTimeout(this.time5);
-        clearTimeout(this.timeFinal);
+        clearTimeout(this.timeFinal1);
+        clearTimeout(this.timeFinal2);
+        clearTimeout(this.timeFinal3);
+        clearTimeout(this.timeFinal4);
+        clearTimeout(this.timeFinal5);
         //la vista le saca la clase de animación a todos los obstaculos
         this.obstaculos.forEach(element => {
             this.viewDivObstaculo.detenerAnimacion(element);
@@ -59,33 +68,33 @@ class ControlDivObstaculos{
         ////////////////SUELO///////////////////
         //primer obstaculo de suelo
             //aplica la animación y esta inicia
-        this.time1=  setTimeout(()=> {this.viewDivObstaculo.aplicarAnimacion(this.divSuelo1, this.tiempoAnimacion)}, 0);
+        this.time1 = setTimeout(()=> {this.viewDivObstaculo.aplicarAnimacion(this.divSuelo1, this.tiempoAnimacion)}, 0);
             //remueve la animacion para poder aplicarse devuelta más tarde
-            setTimeout(()=> {this.viewDivObstaculo.detenerAnimacion(this.divSuelo1);}, this.tiempoAnimacion);//4000 tiempoAnimacion->4
+        this.timeFinal1 = setTimeout(()=> {this.viewDivObstaculo.detenerAnimacion(this.divSuelo1);}, this.tiempoAnimacion);//4000 tiempoAnimacion->4
         ////////////////SUELO///////////////////
         //segundo obstaculo de suelo
             //aplica la animación y esta inicia
-        this.time2=    setTimeout(()=> {this.viewDivObstaculo.aplicarAnimacion(this.divSuelo2, this.tiempoAnimacion);}, this.tiempoAnimacion/2);//2000 tiempoAnimacion->4
+        this.time2 = setTimeout(()=> {this.viewDivObstaculo.aplicarAnimacion(this.divSuelo2, this.tiempoAnimacion);}, this.tiempoAnimacion/2);//2000 tiempoAnimacion->4
             //remueve la animacion para poder aplicarse devuelta más tarde
-            setTimeout(()=> {this.viewDivObstaculo.detenerAnimacion(this.divSuelo2);}, this.tiempoAnimacion + 1500);//5500 tiempoAnimacion->4
+        this.timeFinal2 = setTimeout(()=> {this.viewDivObstaculo.detenerAnimacion(this.divSuelo2);}, this.tiempoAnimacion + 1500);//5500 tiempoAnimacion->4
         ////////////////AIRE///////////////////
         //tercer obstaculo de aire
             //aplica la animación y esta inicia
         this.time3=    setTimeout(()=> {this.viewDivObstaculo.aplicarAnimacion(this.divAire1, this.tiempoAnimacion)}, this.tiempoAnimacion);//4000 tiempoAnimacion->4
             //remueve la animación para poder aplicarse devuelta más tarde
-            setTimeout(()=> {this.viewDivObstaculo.detenerAnimacion(this.divAire1);}, this.tiempoAnimacion*2);//8000 tiempoAnimacion->4
+        this.timeFinal3 = setTimeout(()=> {this.viewDivObstaculo.detenerAnimacion(this.divAire1);}, this.tiempoAnimacion*2);//8000 tiempoAnimacion->4
         ////////////////SUELO///////////////////
         //cuarto obstaculo de suelo
             //aplica la animación y esta inicia
-            this.time4=    setTimeout(()=> {this.viewDivObstaculo.aplicarAnimacion(this.divSuelo1, this.tiempoAnimacion);}, (this.tiempoAnimacion*2)-1000);//7000 tiempoAnimacion->4
+        this.time4 = setTimeout(()=> {this.viewDivObstaculo.aplicarAnimacion(this.divSuelo1, this.tiempoAnimacion);}, (this.tiempoAnimacion*2)-1000);//7000 tiempoAnimacion->4
             //remueve la animacion para poder aplicarse devuelta más tarde
-            setTimeout(()=> {this.viewDivObstaculo.detenerAnimacion(this.divSuelo1);}, (this.tiempoAnimacion*3)-1000);//11000 tiempoAnimacion->4
+        this.timeFinal4 = setTimeout(()=> {this.viewDivObstaculo.detenerAnimacion(this.divSuelo1);}, (this.tiempoAnimacion*3)-1000);//11000 tiempoAnimacion->4
         ////////////////AIRE///////////////////
         //quinto obstaculo de aire
             //aplica la animación y esta inicia
-            this.time5=    setTimeout(()=> {this.viewDivObstaculo.aplicarAnimacion(this.divAire1, this.tiempoAnimacion);}, (this.tiempoAnimacion*2)+1000);//9000 tiempoAnimacion->4
+        this.time5 = setTimeout(()=> {this.viewDivObstaculo.aplicarAnimacion(this.divAire1, this.tiempoAnimacion);}, (this.tiempoAnimacion*2)+1000);//9000 tiempoAnimacion->4
             //remueve la animacion para poder aplicarse devuelta más tarde
-        this.timeFinal=    setTimeout(()=> {this.viewDivObstaculo.detenerAnimacion(this.divAire1);
+        this.timeFinal5 = setTimeout(()=> {this.viewDivObstaculo.detenerAnimacion(this.divAire1);
                 //vuelve a ejecutar la misa secuencia de animación
             this.secuenciaAnimacion();}, (this.tiempoAnimacion*3)+1000);//13000 tiempoAnimacion->4
         }
